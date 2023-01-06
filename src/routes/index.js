721892d7f31f1a405f8a2cd220a6c8577de0cedf
AmazingShadow <amazingshadoww@gmail.com>
2023-01-06 11:08:09 +0700

    tao trang chi tiet hoa hoc va load du lieu len

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
 const lecturersRouter = require('./lecturers');
+const coursesRouter = require('./courses');
 
 function route(app) {
     app.use('/news', newsRouter);
     app.use('/lecturers', lecturersRouter);
+    app.use('/courses', coursesRouter);
     app.use('/', siteRouter);
 }
 
